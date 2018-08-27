4de27a2eec4e8c298699cda1bb0eaa7d89630b54
Jeffrey Richman <jrichman2009@gmail.com>
2018-08-27 02:17:27 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,11 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
+    if @roster[grade].size == 0
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+    @roster[grade] = [@roster[grade]] << name
   end
 
 end
